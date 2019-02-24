1175c4275af0a4a9da9a77141e2ad19490920d8e
eqyfys <p.pribora@gmail.com>
2019-02-24 21:13:33 +0300

    Update query.sql

--- HW03/query.sql
+++ HW03/query.sql
@@ -1,30 +1,30 @@
-1)
-a)
+--1)
+--a)
   SELECT PersonID, FullName FROM Application.People 
     WHERE IsSalesperson = 1
-	AND PersonID NOT IN (SELECT SalespersonPersonID FROM Sales.Invoices)
+	AND PersonID NOT IN (SELECT DISTINCT SalespersonPersonID FROM Sales.Invoices);
 
-b)
+--b)
 WITH SalesPeople AS
 (
   SELECT  SalespersonPersonID FROM Sales.Invoices
 )
   SELECT p.PersonID, p.FullName FROM Application.People as p
    WHERE p.IsSalesperson = 1
-   AND  NOT EXISTS (SELECT * FROM SalesPeople WHERE SalespersonPersonID = p.PersonID)
+   AND  NOT EXISTS (SELECT 1 FROM SalesPeople WHERE SalespersonPersonID = p.PersonID);
 
-2)
-a)
+--2)
+--a)
   SELECT StockItemID, StockItemName, UnitPrice
    FROM Warehouse.StockItems 
-   WHERE UnitPrice = (SELECT MIN (UnitPrice) FROM Warehouse.StockItems)
+   WHERE UnitPrice = (SELECT MIN (UnitPrice) FROM Warehouse.StockItems);
 
-b)
+--b)
   SELECT StockItemID, StockItemName, UnitPrice
    FROM Warehouse.StockItems 
-   WHERE UnitPrice = (SELECT TOP 1 UnitPrice FROM Warehouse.StockItems ORDER BY UnitPrice)
+   WHERE UnitPrice = (SELECT TOP 1 UnitPrice FROM Warehouse.StockItems ORDER BY UnitPrice);
  
- c)
+-- c)
  WITH MinPrice AS 
  (
     SELECT MIN (UnitPrice) as Price FROM Warehouse.StockItems
@@ -32,26 +32,27 @@ b)
    
  SELECT StockItemID, StockItemName, UnitPrice
    FROM Warehouse.StockItems 
-   WHERE UnitPrice = (SELECT Price FROM  MinPrice)
+   WHERE UnitPrice = (SELECT Price FROM  MinPrice);
 
-3)
-a)
- SELECT CustomerID, CustomerName
+--3)
+--a)
+ SELECT DISTINCT CustomerID, CustomerName
   FROM Sales.Customers
-   WHERE CustomerID IN (SELECT TOP 5 CustomerID FROM Sales.CustomerTransactions ORDER BY TransactionAmount DESC)
+   WHERE CustomerID IN (SELECT TOP 5 CustomerID FROM Sales.CustomerTransactions ORDER BY TransactionAmount DESC);
 
-b)
-  SELECT DISTINCT * FROM 
+--b)
+  SELECT * FROM 
 	  (SELECT TOP 5  ct.CustomerID,
 			   cu.CustomerName
 	  FROM Sales.CustomerTransactions as ct
-	  JOIN (SELECT CustomerID, CustomerName FROM Sales.Customers) as cu
+	  JOIN (SELECT  CustomerID, CustomerName FROM Sales.Customers) as cu
 		   ON cu.CustomerID = ct.CustomerID
 	  GROUP BY ct.CustomerID, cu.CustomerName, ct.TransactionAmount
 	  ORDER BY ct.TransactionAmount DESC
-	 )  as tbl
+	 )  as tbl;
 
-c)
+
+--c)
  WITH MaxTrans AS 
  (
    SELECT TOP 5 CustomerID FROM Sales.CustomerTransactions ORDER BY TransactionAmount DESC
@@ -59,21 +60,21 @@ c)
  
   SELECT DISTINCT cu.CustomerID, cu.CustomerName
   FROM Sales.Customers as cu
-  JOIN MaxTrans as mt ON mt.CustomerID = cu.CustomerID
+  JOIN MaxTrans as mt ON mt.CustomerID = cu.CustomerID;
 
-4)
-a)
+--4)
+--a)
 SELECT DISTINCT c.CityID,
        c.CityName,
-	  (SELECT FullName FROM Application.People WHERE PersonID = i.PackedByPersonID) as Supplier
+       p.FullName as Supplier
  FROM Application.Cities as c
  JOIN Sales.Customers as cu ON cu.DeliveryCityID = c.CityID
  JOIN  Sales.Invoices as i ON i.CustomerID = cu.CustomerID
  JOIN Sales.InvoiceLines as il ON il.InvoiceID = i.InvoiceID
- JOIN Warehouse.StockItems as si ON si.StockItemID = il.StockItemID
- WHERE si.StockItemID IN (SELECT TOP 3 StockItemID  FROM Warehouse.StockItems ORDER BY  UnitPrice DESC)
+ JOIN Application.People as p ON p.PersonID = i.PackedByPersonID
+ WHERE il.StockItemID IN (SELECT TOP 3 StockItemID  FROM Warehouse.StockItems ORDER BY  UnitPrice DESC);
 
- b)
+ --b)
  WITH MostExpensiveItems as 
  (
    SELECT TOP 3 StockItemID  FROM Warehouse.StockItems ORDER BY  UnitPrice DESC
@@ -81,13 +82,13 @@ SELECT DISTINCT c.CityID,
 
  SELECT DISTINCT c.CityID,
        c.CityName,
-	  (SELECT FullName FROM Application.People WHERE PersonID = i.PackedByPersonID) as Supplier
+       p.FullName as Supplier
  FROM Application.Cities as c
  JOIN Sales.Customers as cu ON cu.DeliveryCityID = c.CityID
  JOIN  Sales.Invoices as i ON i.CustomerID = cu.CustomerID
  JOIN Sales.InvoiceLines as il ON il.InvoiceID = i.InvoiceID
- JOIN Warehouse.StockItems as si ON si.StockItemID = il.StockItemID
- JOIN MostExpensiveItems as mei ON mei.StockItemID = si.StockItemID
+ JOIN Application.People as p ON p.PersonID = i.PackedByPersonID
+ JOIN MostExpensiveItems as mei ON mei.StockItemID = il.StockItemID;
  
  
 
