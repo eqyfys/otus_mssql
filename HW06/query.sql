99e38d419efb893e992bfb5a3cc3ae7b5b5ffe06
eqyfys <p.pribora@gmail.com>
2019-02-26 21:49:08 +0300

    Update query.sql

--- HW06/query.sql
+++ HW06/query.sql
@@ -6,11 +6,10 @@
 --a) with windows function
 SELECT i.InvoiceID,
        c.CustomerName,
-		   i.InvoiceDate,
-	     ct.TransactionAmount as Summ,
-		   SUM(ct.TransactionAmount) 
-           OVER (ORDER BY DATEPART(year, i.InvoiceDate),
-           DATEPART(month, i.InvoiceDate)) as RunningPerMonthSumm
+       i.InvoiceDate,
+       ct.TransactionAmount as Summ,
+       SUM(ct.TransactionAmount) OVER (ORDER BY DATEPART(year, i.InvoiceDate),
+       DATEPART(month, i.InvoiceDate)) as RunningPerMonthSumm
 FROM  Sales.Invoices as i
 JOIN Sales.CustomerTransactions as ct ON ct.InvoiceID = i.InvoiceID
 JOIN Sales.Customers as c ON c.CustomerID = ct.CustomerID
@@ -19,14 +18,14 @@ ORDER BY i.InvoiceDate
 
  --b) without windows function
 SELECT 
-	    i.InvoiceID,
+      i.InvoiceID,
       c.CustomerName,
-		  i.InvoiceDate,
-	    ct.TransactionAmount as Summ,
-		  (SELECT SUM(TransactionAmount) FROM Sales.CustomerTransactions 
-		      WHERE InvoiceID IN (SELECT InvoiceID FROM Sales.Invoices 
-			                      WHERE InvoiceDate BETWEEN '2015-01-01' 
-								  AND EOMONTH(i.InvoiceDate))) as RunningPerMonthSumm
+      i.InvoiceDate,
+      ct.TransactionAmount as Summ,
+      (SELECT SUM(TransactionAmount) FROM Sales.CustomerTransactions 
+       WHERE InvoiceID IN (SELECT InvoiceID FROM Sales.Invoices 
+			    WHERE InvoiceDate BETWEEN '2015-01-01' 
+			    AND EOMONTH(i.InvoiceDate))) as RunningPerMonthSumm
 FROM  Sales.Invoices as i
 JOIN Sales.CustomerTransactions as ct ON ct.InvoiceID = i.InvoiceID
 JOIN Sales.Customers as c ON c.CustomerID = ct.CustomerID
