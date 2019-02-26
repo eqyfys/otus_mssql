3eeb0dcdb53a4d40b71fc70f5a61454a25ee2b71
eqyfys <p.pribora@gmail.com>
2019-02-26 13:11:13 +0300

    Update and rename HWO4/query.sql to HWO5/query.sql

--- HWO5/query.sql
+++ HWO5/query.sql
@@ -2,33 +2,33 @@
 SELECT YEAR(i.InvoiceDate) as Years,
        DATENAME(month, i.InvoiceDate) as Months,
        AVG(il.UnitPrice) as AveragePrice,
-	     SUM(il.UnitPrice*il.Quantity) as AllSumm
+       SUM(il.UnitPrice*il.Quantity) as AllSumm
 FROM Sales.Invoices as i
 JOIN Sales.InvoiceLines as il ON il.InvoiceID = i.InvoiceID
 GROUP BY YEAR(i.InvoiceDate),  DATENAME(month, i.InvoiceDate)
-ORDER BY  YEAR(i.InvoiceDate),  DATENAME(month, i.InvoiceDate);
+ORDER BY  Years,  Months;
 
 -- 2)
 SELECT  YEAR(i.InvoiceDate) as Years,
         DATENAME(month, i.InvoiceDate) as Months,
         AVG(il.UnitPrice) as AveragePrice,
-	      SUM(il.UnitPrice*il.Quantity) as AllSumm
+	SUM(il.UnitPrice*il.Quantity) as AllSumm
 FROM Sales.Invoices as i
 JOIN Sales.InvoiceLines as il ON il.InvoiceID = i.InvoiceID
 GROUP BY YEAR(i.InvoiceDate),  DATENAME(month, i.InvoiceDate)
 HAVING  SUM(il.UnitPrice*il.Quantity) > 10000
-ORDER BY  YEAR(i.InvoiceDate),  DATENAME(month, i.InvoiceDate);
+ORDER BY  Years,  Months;
    
  --3)
 SELECT YEAR(i.InvoiceDate) as Years,
        DATENAME(month, i.InvoiceDate) as Months,
-	     il.StockItemID,
-	     il.Description,
-       SUM(il.Quantity) as ItemsCount,
-	     SUM(il.UnitPrice*il.Quantity) as AllSumm,
-	     MIN(i.InvoiceDate) as FirstSaleDate
+	il.StockItemID,
+	il.Description,
+        SUM(il.Quantity) as ItemsCount,
+	SUM(il.UnitPrice*il.Quantity) as AllSumm,
+        MIN(i.InvoiceDate) as FirstSaleDate
 FROM Sales.Invoices as i
 JOIN Sales.InvoiceLines as il ON il.InvoiceID = i.InvoiceID
 GROUP BY YEAR(i.InvoiceDate),  DATENAME(month, i.InvoiceDate),	il.StockItemID, il.Description
 HAVING  SUM(il.Quantity)  < 50
-ORDER BY  YEAR(i.InvoiceDate),  DATENAME(month, i.InvoiceDate), il.StockItemID, il.Description;
+ORDER BY  Years, Months, il.StockItemID, il.Description;
