e32512c41dbb410cd7ffce7afa05b29042638296
eqyfys <p.pribora@gmail.com>
2019-03-06 19:50:29 +0300

    Update query.sql

--- HW07/query.sql
+++ HW07/query.sql
@@ -46,4 +46,65 @@ SELECT * FROM @CustomersSumm
 --Оптимизация этих запросов в первую очередь заключалась бы, полагаю, 
 -- в создании ключа по InvoceID при создании таблицы или табличной переменной. 							 
 							 
-							 
+--2)
+--a)with temp table
+CREATE TABLE #ResultTable (EmployeeID int, Name nvarchar(100), Title nvarchar(50), EmployeeLevel int);
+
+WITH Example_CTE (EmployeeID, Name, Title, EmployeeLevel) AS
+(
+   SELECT EmployeeID, 
+          CAST((FirstName + ' ' + LastName) as nvarchar(100)) as Name,
+          Title,
+          1 as EmployeeLevel
+   FROM dbo.MyEmployees
+   WHERE ManagerID IS NULL
+   UNION ALL
+   SELECT e.EmployeeID, 
+          CASE WHEN EmployeeLevel = 1
+          THEN CAST('|' as nvarchar(3))  + CAST((FirstName + ' ' + LastName) as nvarchar(97))
+          WHEN  EmployeeLevel = 2
+          THEN CAST('||' as nvarchar(3))  + CAST((FirstName + ' ' + LastName) as nvarchar(97))
+          WHEN EmployeeLevel = 3
+          THEN CAST('|||' as nvarchar(3))  + CAST((FirstName + ' ' + LastName) as nvarchar(97))
+          END 
+          as Name,
+          e.Title,
+          EmployeeLevel + 1
+   FROM dbo.MyEmployees as e 
+       INNER JOIN Example_CTE as ex 
+       ON e.ManagerID = ex.EmployeeID 
+)
+
+INSERT INTO #ResultTable (EmployeeID, Name, Title, EmployeeLevel)
+SELECT EmployeeID, Name, Title, EmployeeLevel FROM Example_CTE;
+		    
+--b) with table variable
+DECLARE @ResultTable TABLE (EmployeeID int, Name nvarchar(100), Title nvarchar(50), EmployeeLevel int);
+
+WITH Example_CTE (EmployeeID, Name, Title, EmployeeLevel) AS
+(
+   SELECT EmployeeID, 
+          CAST((FirstName + ' ' + LastName) as nvarchar(100)) as Name,
+          Title,
+          1 as EmployeeLevel
+   FROM dbo.MyEmployees
+   WHERE ManagerID IS NULL
+   UNION ALL
+   SELECT e.EmployeeID, 
+          CASE WHEN EmployeeLevel = 1
+          THEN CAST('|' as nvarchar(3))  + CAST((FirstName + ' ' + LastName) as nvarchar(97))
+          WHEN  EmployeeLevel = 2
+          THEN CAST('||' as nvarchar(3))  + CAST((FirstName + ' ' + LastName) as nvarchar(97))
+          WHEN EmployeeLevel = 3
+          THEN CAST('|||' as nvarchar(3))  + CAST((FirstName + ' ' + LastName) as nvarchar(97))
+          END 
+          as Name,
+          e.Title,
+          EmployeeLevel + 1
+   FROM dbo.MyEmployees as e 
+       INNER JOIN Example_CTE as ex 
+        ON e.ManagerID = ex.EmployeeID 
+)
+
+INSERT INTO @ResultTable (EmployeeID, Name, Title, EmployeeLevel)
+SELECT EmployeeID, Name, Title, EmployeeLevel FROM Example_CTE;
