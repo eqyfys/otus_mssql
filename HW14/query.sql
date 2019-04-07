19826c887aa7d492879a1816b6c8a6a16920792b
eqyfys <p.pribora@gmail.com>
2019-04-07 20:05:14 +0300

    Update query.sql

--- HW14/query.sql
+++ HW14/query.sql
@@ -14,3 +14,18 @@ BEGIN
 	RETURN @Result
 END
 GO
+
+--2)
+
+CREATE PROCEDURE [Sales].[GetSummByCustomerID]
+	@CustomerID int
+AS
+BEGIN
+   SELECT SUM(il.UnitPrice*il.Quantity) as InvoiceSumm
+   FROM Sales.InvoiceLines as il
+   JOIN Sales.Invoices as i ON i.InvoiceID = il.InvoiceID
+   JOIN Sales.Customers as c ON c.CustomerID = i.CustomerID
+   WHERE c.CustomerID = @CustomerID
+END
+GO
+
